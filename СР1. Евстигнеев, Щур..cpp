a070e6f9371e124f84943200bfcae6e6824aa6ad
lsalsl <115686826+lsalsl@users.noreply.github.com>
2022-10-13 08:42:30 +0300

    Add files via upload

---
+++ СР1. Евстигнеев, Щур..cpp
@@ -0,0 +1,222 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+	setlocale(0, "");
+	
+	int z = 13;
+	cout << "z = "<< z << " | тип: int, размер: 4 б. " << endl;
+
+	int i_1 = 11;
+	cout << "i_1 = "<<i_1<< " | тип: int, размер: 4 б. " << endl;
+
+	int g_0 = 7;
+	cout << "g_0 = " << g_0 << " | тип: int, размер: 4 б. " << endl;
+
+	int f_1 = 3;
+	cout << "f_1 = " << f_1 << " | тип: int, размер: 4 б. " << endl;
+
+	int b = 4;
+	cout << "b = " << b << " | тип: int, размер: 4 б. " << endl;
+
+	int c_4 = 11;
+	cout << "c_4 = " << c_4 << " | тип: int, размер: 4 б. " << endl;
+
+	int d_2 = 9;
+	cout << "d_2 = " << d_2 << " | тип: int, размер: 4 б. " << endl;
+
+	int h_9 = 17;
+	cout << "h_9 = " << h_9 << " | тип: int, размер: 4 б. " << endl;
+
+	int o = 1;
+	cout << "o = " << o << " | тип: int, размер: 4 б. " << endl;
+
+	int l = 1;
+	cout << "l = " << l << " | тип: int, размер: 4 б. " << endl;
+
+	float z_3 = 1.2;
+	cout << "z_3 = " << z_3 << " | тип: float, размер: 4 б. " << endl;
+
+	float v_3 = 5.6;
+	cout << "v_3 = " << v_3 << " | тип: float, размер: 4 б. " << endl;
+
+	float q_7 = 2.3;
+	cout << "q_7 = " << q_7 << " | тип: float, размер: 4 б. " << endl;
+
+	float x_4 = 1.9;
+	cout << "x_4 = " << x_4 << " | тип: float, размер: 4 б. " << endl;
+
+	float p = 6.2;
+	cout << "p = " << p << " | тип: float, размер: 4 б. " << endl;
+
+	float y = 5.5;
+	cout << "y = " << y << " | тип: float, размер: 4 б. " << endl;
+	
+	float j = 8.8;
+	cout << "j = " << j << " | тип: float, размер: 4 б. " << endl;
+
+	float m = 6.4;
+	cout << "m = " << m << " | тип: float, размер: 4 б. " << endl;
+
+	float n = 0.9;
+	cout << "n = " << n << " | тип: float, размер: 4 б. " << endl;
+
+	double p_7 = 1.134;
+	cout << "p_7 = " << p_7 << " | тип: double, размер: 8 б. " << endl;
+
+	double x_3 = 7.666;
+	cout << "x_3 = " << x_3 << " | тип: double, размер: 8 б. " << endl;
+
+	double i_8 = 6.666;
+	cout << "i_8 = " << i_8 << " | тип: double, размер: 8 б. " << endl;
+
+	double g_5 = 5.952;
+	cout << "g_5 = " << g_5 << " | тип: double, размер: 8 б. " << endl;
+
+	double p_9 = 8.114;
+	cout << "p_9 = " << p_9 << " | тип: double, размер: 8 б. " << endl;
+
+	double f_7 = 2.994;
+	cout << "f_7 = " << f_7 << " | тип: double, размер: 8 б. " << endl;
+
+	double c_0 = 3.676;
+	cout << "c_0 = " << c_0 << " | тип: double, размер: 8 б. " << endl;
+
+	double h_8 = 9.887;
+	cout << "h_8 = " << h_8 << " | тип: double, размер: 8 б. " << endl;
+
+	double d_9 = 1.970;
+	cout << "d_9 = " << d_9 << " | тип: double, размер: 8 б. " << endl;
+
+	double e_7 = 3.224;
+	cout << "e_7 = " << e_7 << " | тип: double, размер: 8 б. " << endl;
+
+	long ab_7 = 231;
+	cout << "ab_7 = " << ab_7 << " | тип: long, размер: 4 б. " << endl;
+
+	long lp_9 = 132;
+	cout << "lp_9 = " << lp_9 << " | тип: long, размер: 4 б. " << endl;
+
+	long bm_23 = 111;
+	cout << "bm_23 = " << bm_23 << " | тип: long, размер: 4 б. " << endl;
+
+	long dq_6 = 43;
+	cout << "dq_6 = " << dq_6 << " | тип: long, размер: 4 б. " << endl;
+
+	long qk_1 = 67;
+	cout << "qk_1 = " << qk_1 << " | тип: long, размер: 4 б. " << endl;
+	
+	long cy_2 = 321;
+	cout << "cy_2 = " << cy_2 << " | тип: long, размер: 4 б. " << endl;
+	
+	long db_6 = 15;
+	cout << "db_6 = " << db_6 << " | тип: long, размер: 4 б. " << endl;
+	
+	long oot_8 = 89;
+	cout << "oot_8 = " << oot_8 << " | тип: long, размер: 4 б. " << endl;
+	
+	long bcd_9 = 54;
+	cout << "bcd_9 = " << bcd_9 << " | тип: long, размер: 4 б. " << endl;
+	
+	long zxc_5 = 100;
+	cout << "zxc_5 = " << zxc_5 << " | тип: long, размер: 4 б. " << endl;
+
+	short cq = 13;
+	cout << "cq = " << cq << " | тип: short, размер: 2 б. " << endl;
+
+	short qq_3 = 5;
+	cout << "qq_3 = " << qq_3 << " | тип: short, размер: 2 б. " << endl;
+
+	short gm_2 = 9;
+	cout << "gm_2 = " << gm_2 << " | тип: short, размер: 2 б. " << endl;
+
+	short gg = 5;
+	cout << "gg = " << gg << " | тип: short, размер: 2 б. " << endl;
+
+	short mm = 7;
+	cout << "mm = " << mm << " | тип: short, размер: 2 б. " << endl;
+
+	short ot  = 2;
+	cout << "ot = " << ot << " | тип: short, размер: 2 б. " << endl;
+
+	short pm = 1;
+	cout << "pm = " << pm << " | тип: short, размер: 2 б. " << endl;
+
+	short af = 6;
+	cout << "af = " << af << " | тип: short, размер: 2 б. " << endl;
+
+	short lk = 4;
+	cout << "lk = " << lk << " | тип: short, размер: 2 б. " << endl;
+
+	short bn = 2;
+	cout << "bn = " << bn << " | тип: short, размер: 2 б. " << endl;
+
+	char ny = '0';
+	cout << "ny = " << ny << " | тип: char, размер: 1 б. " << endl;
+
+	char bb = '1';
+	cout << "bb = " << bb << " | тип: char, размер: 1 б. " << endl;
+
+	char afk = '2';
+	cout << "afk = " << afk << " | тип: char, размер: 1 б. " << endl;
+
+	char ggg = 'f';
+	cout << "ggg = " << ggg << " | тип: char, размер: 1 б. " << endl;
+
+	char lsk = 'd';
+	cout << "lsk = " << lsk << " | тип: char, размер: 1 б. " << endl;
+
+	char pkg = 'a';
+	cout << "pkg = " << pkg << " | тип: char, размер: 1 б. " << endl;
+
+	char lgt = '4';
+	cout << "lgt = " << lgt << " | тип: char, размер: 1 б. " << endl;
+
+	char cc_1 = '7';
+	cout << "cc_1 = " << cc_1 << " | тип: char, размер: 1 б. " << endl;
+
+	char of_3 = '6';
+	cout << "of_3 = " << of_3 << " | тип: char, размер: 1 б. " << endl;
+
+	char ppt_1 = '9';
+	cout << "ppt_1 = " << ppt_1 << " | тип: char, размер: 1 б. " << endl;
+
+	bool a_1 = 0;
+	cout << "a_1 = " << a_1 << " | тип: bool, размер: 1 б. " << endl;
+	
+	bool f_8 = 1;
+	cout << "f_8 = " << f_8 << " | тип: bool, размер: 1 б. " << endl;
+
+	bool pk_1 = -50;
+	cout << "pk_1 = " << pk_1 << " | тип: bool, размер: 1 б." << endl;
+
+	bool ch_5 = -134;
+	cout << "ch_5 = " << ch_5 << " | тип: bool, размер: 1 б." << endl;
+
+	bool shur_0 = 13;
+	cout << "shur_0 = " << shur_0 << " | тип: bool, размер: 1 б." << endl;
+
+	bool bnc_1 = 12;
+	cout << "bnc_1 = " << bnc_1 << " | тип: bool, размер: 1 б." << endl;
+
+	bool kls_2 = 17;
+	cout << "kls_2 = " << kls_2 << " | тип: bool, размер: 1 б." << endl;
+
+	bool lop_1 = 16;
+	cout << "lop_1 = " << lop_1 << " | тип: bool, размер: 1 б." << endl;
+
+	bool jek_2 = 11;
+	cout << "jek_2 = " << jek_2 << " | тип: bool, размер: 1 б." << endl;
+
+	bool evst_0 = 19;
+	cout << "evst_0 = " << evst_0 << " | тип: bool, размер: 1 б." << endl;
+
+	return 0;
+}
+//Работа с переменными и типами данных. Инициализация каждого типа данных по 10 раз. Вывод всех переменных в консоль.
+//int a_1 = 10;
+//cout <<"a_1 = "<< a_1 << " | тип: int, размер: 4 б. " << endl;
+
+
+
+
